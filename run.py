e9c34eb9fbc3e499780f989e372ff131c140ea75
jarvis73 <zjw.math@qq.com>
2022-12-11 16:14:36 +0800

    fix: KeyError when disabling precompute_weights

--- run.py
+++ run.py
@@ -44,7 +44,9 @@ class Trainer(BaseTrainer):
         qry_rgb = batch['qry_rgb'].cuda()
         qry_msk = batch['qry_msk'].cuda()
         classes = batch['cls'].cuda()
-        kwargs = {'weight': batch['weights'].cuda()}
+        kwargs = {}
+        if 'weights' in batch:
+            kwargs['weight'] = batch['weights'].cuda()
 
         output = self.model_DP(qry_rgb, sup_rgb, sup_msk, qry_msk)
         qry_msk_reshape = qry_msk.view(-1, *qry_msk.shape[-2:])
